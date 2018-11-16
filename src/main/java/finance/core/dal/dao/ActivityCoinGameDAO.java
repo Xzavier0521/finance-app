0264dadac6b53b9754c1d249c584561178074e61
ApeCoder <hbtcsnlili@gmail.com>
2018-11-16 13:54:58 +0800

    红包雨活动

---
+++ src/main/java/finance/core/dal/dao/ActivityCoinGameDAO.java
@@ -0,0 +1,29 @@
+package finance.core.dal.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import finance.core.dal.dataobject.ActivityCoinGameDO;
+/**
+ * <p>注释</p>
+ * @author lili
+ * @version $Id: ActivityCoinGameDAO.java, v0.1 2018/11/15 9:12 PM lili Exp $
+ */
+public interface ActivityCoinGameDAO {
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(ActivityCoinGameDO record);
+
+    int insertSelective(ActivityCoinGameDO record);
+
+    List<ActivityCoinGameDO> query(Map parameters);
+
+    int count(Map parameters);
+
+    ActivityCoinGameDO selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(ActivityCoinGameDO record);
+
+    int updateByPrimaryKey(ActivityCoinGameDO record);
+}
