7449647a8f3ca099b781db22316a1cc2a0bf491f
ApeCoder <hbtcsnlili@gmail.com>
2018-11-20 19:23:55 +0800

    no message

---
+++ src/main/java/finance/domainservice/service/activity/RedEnvelopeRainNoticeService.java
@@ -0,0 +1,12 @@
+package finance.domainservice.service.activity;
+
+/**
+ * <p>红包雨活动推送通知</p>
+ *
+ * @author lili
+ * @version 1.0: RedEnvelopeRainNoticeService.java, v0.1 2018/11/20 6:56 PM PM lili Exp $
+ */
+public interface RedEnvelopeRainNoticeService {
+
+    void process();
+}
